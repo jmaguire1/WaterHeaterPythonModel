0486e61364da42598231f4e4462cc95c6f09b987
cbooten <chuck.booten@nrel.gov>
2017-11-09 13:07:22 -0700

    adding fleet control script and simple water heater model to interface with

--- WH_control_fleet_CWB.py
+++ WH_control_fleet_CWB.py
@@ -11,7 +11,7 @@ from simple_wh_cwb_2 import ChuckWaterHeater
 import random
 
 def main():
-    numWH = 1 #number of water heaters in fleet
+    numWH = 200 #number of water heaters in fleet
     TtankInitialMean = 125 #deg F
     TtankInitialStddev = 5 #deg F
     TsetInitialMean = 125 #deg F
@@ -21,8 +21,9 @@ def main():
     CapacityMasterList = [50,50,50,50,50,50,50,50,40,40,80] #70% 50 gal, 20% 40 gal, 10% 80 gal
     TypeMasterList = ['ER','ER','ER','ER','ER','ER','ER','ER','ER','HP'] #elec_resis 90% and HPWH 10%
     LocationMasterList =['Conditioned','Conditioned','Conditioned','Conditioned','Unconditioned'] #80% conditioned, 20% unconditioned
-    HotDrawMasterList = [1.5,1.1,0.7,0.2,0.05,0.05,0.02,0.01,0.01,0.01,0,0,0,0,0,0,0,0,0,0,0,0,0,0]#sample hourly draw pattern. will randomly sample this 
+    HotDrawMasterList = [0.4,0.3,0.2, 0.1, 0.05,0.05,0.02,0.01,0.01,0.01,0,0,0,0,0,0,0,0,0,0,0,0,0,0]#sample hourly draw pattern. will randomly sample this 
     # 1x for each house and assume pattern is the same for that house for the entire year
+    MaxServiceCallMasterList = [100,80,80, 200, 150, 110, 50, 75, 100]
     
         
         
@@ -30,7 +31,7 @@ def main():
     fleet_load_request = []
 #    fleet_regulation_request = []
     for hour in range(8760):
-        magnitude_load_add_shed = 2e6 + 1e6*random.random() #def magnitude of request for load add/shed
+        magnitude_load_add_shed = (1e6 + 5e5*random.random())/numWH #def magnitude of request for load add/shed
         if hour % 12 == 0 or hour % 12 == 1 or hour % 12 == 2:
             service = ['load shed',magnitude_load_add_shed]
         elif hour % 7 == 0 or hour % 7 == 1:
@@ -40,7 +41,6 @@ def main():
             
         fleet_load_request.append(service)
         
-    
     
 #    #define regulation request separately since timescale is very different
 #    for second in range(1000):
@@ -66,6 +66,9 @@ def main():
     Type = [random.choice(TypeMasterList) for n in range(numWH)]
     Location = [random.choice(LocationMasterList) for n in range(numWH)]
     Hot_draw = np.array([[random.choice(HotDrawMasterList) for h in range(24)] for i in range(numWH)])
+    MaxServiceCalls = [random.choice(MaxServiceCallMasterList) for n in range(numWH)]
+    
+#    print(MaxServiceCalls)
     
     (Tamb, RHamb, Tmains, hot_draw) = get_annual_conditions()
     
@@ -91,59 +94,49 @@ def main():
     
     Tset = [[0 for x in range(8760)] for y in range(numWH)]
     Ttank = [[0 for x in range(8760)] for y in range(numWH)]
-#    Eused = []
-#    Eloss = []
-#    ElementOn = []
-#    Eservice = []
     SoC = [[0 for x in range(8760)] for y in range(numWH)]
     AvailableCapacity = [[0 for x in range(8760)] for y in range(numWH)]
     ServiceCallsAccepted = [[0 for x in range(8760)] for y in range(numWH)]
-    MaxServiceCalls = [0  for y in range(numWH)]
-#    wh_attributes = [[0 for x in range(8760)] for y in range(numWH)]
-    
-        
-#            if hour == 1: #assign initial values TtankInitial, TsetInitial, and WH characteristics: Capacity, Type, Location and Hot_draw
-    whs = [ChuckWaterHeater(TsetInitial[number], Tamb[1], RHamb[1], Tmains[1], Hot_draw[number,1], fleet_load_request[1], Capacity[number], Type[number], Location[number], 0, MaxServiceCalls[number]) for number in range(numWH)]
-#                tset, ttank, eused, eloss, elementOn, eservice, soC, availableCapacity = wh.execute()
-#            else:
-#                wh = ChuckWaterHeater(wh_attributes[number,hour-1], Tamb[hour], RHamb[hour], Tmains[hour], Hot_draw[number,hour], fleet_load_request[hour], Ttank[number,hour -1], Capacity[number], Type[number], Location[number])
-                
-    for hour in range(7):    
+    ServiceProvided = [[0 for x in range(8760)] for y in range(numWH)]
+    TotalServiceProvidedPerWH = [0 for y in range(numWH)]
+    TotalServiceProvidedPerTimeStep = [0 for y in range(8760)]
+    TotalServiceCallsAcceptedPerWH = [0 for y in range(numWH)]
+
+    whs = [ChuckWaterHeater(Tamb[1], RHamb[1], Tmains[1], Hot_draw[number,1], fleet_load_request[1], Capacity[number], Type[number], Location[number], 0, MaxServiceCalls[number]) for number in range(numWH)]
+               
+    for hour in range(8760):    
         number = 0
-#        print(hour)
         for wh in whs: #numWH
             if hour == 0:
-                ttank, tset, soC, availableCapacity, serviceCallsAccepted = wh.execute(TsetInitial[number], Tamb[0], RHamb[0], Tmains[0], Hot_draw[number,0], fleet_load_request[0], TtankInitial[number])
-                print('tank initial', TtankInitial[number])
+                ttank, tset, soC, availableCapacity, serviceCallsAccepted, eservice = wh.execute(TtankInitial[number], TsetInitial[number], Tamb[0], RHamb[0], Tmains[0], Hot_draw[number,0], fleet_load_request[0], 0)
             else:
                 lastHour = hour - 1
                 TsetLast = Tset[number][lastHour]
-                TtankLast = Ttank[number][lastHour]-5*hour
+                TtankLast = Ttank[number][lastHour]
+#                print(TtankLast)
                 HourOfDay = hour % 24
-#                print(Hot_draw[number,HourOfDay])
-                print(TtankLast, hour)
-                ttank, tset, soC, availableCapacity, serviceCallsAccepted = wh.execute(TsetLast, Tamb[hour], RHamb[hour], Tmains[hour], Hot_draw[number,HourOfDay], fleet_load_request[hour], TtankLast)
-#                print(ttank)
-                
-            
-#            print(Ttank[number][hour])
+                ttank, tset, soC, availableCapacity, serviceCallsAccepted, eservice = wh.execute(TtankLast, TsetLast, Tamb[hour], RHamb[hour], Tmains[hour], Hot_draw[number,HourOfDay], fleet_load_request[hour],  ServiceCallsAccepted[number][lastHour])
             
             Tset[number][hour] = tset
+#            print(tset)
             Ttank[number][hour] = ttank
-#            print(Tset[number][hour])
             SoC[number][hour] = soC
             AvailableCapacity[number][hour] = availableCapacity
             ServiceCallsAccepted[number][hour] = serviceCallsAccepted
+            ServiceProvided[number][hour] = eservice
+            TotalServiceProvidedPerWH[number] = TotalServiceProvidedPerWH[number] + ServiceProvided[number][hour]
+            TotalServiceProvidedPerTimeStep[hour] = TotalServiceProvidedPerTimeStep[hour] + ServiceProvided[number][hour]
+#            print(Ttank[number][hour])
             number += 1
+    
+    for n in range(number):
+        TotalServiceCallsAcceptedPerWH[n] = ServiceCallsAccepted[n][hour]
         #analysis   
 #        for wh in whs:
 #            wh_thistory[wh,:] = np.array(wh.temp_history)  
             
                 
-                
-    
-#    print(len(Ttank))
-    print(Ttank[0][0:2])
+#    print(Ttank[0][0:2])
        
     
     
@@ -152,19 +145,60 @@ def main():
 #    Ttank, Eused, Eloss, ElementOn, Eservice = wh.execute_year()
     # print(Ttank[1:50], Eused[1:50], Eloss[1:50], control[1:50])
     
+#    print(len(Ttank[0][:]))
+    plt.figure(1)
+    plt.clf()
+    plt.plot(Ttank[0][0:20],'r')
+    plt.plot(Ttank[1][0:20],'b')
+    plt.plot(Ttank[2][0:20],'k')
+    plt.ylabel('Tank Temperature deg F')
+    plt.xlabel('Hour')
     
-#    plt.figure(1)
-#    plt.clf()
-#    plt.plot(Ttank[1:10][20])
-#    
-#    plt.figure(2)
-#    plt.clf()
-#    plt.plot(SoC[1:10][20])
-#    plt.show()
+    plt.figure(2)
+    plt.clf()
+    plt.plot(SoC[0][0:20],'rs-')
+    plt.plot(SoC[1][0:20],'b*-')
+    plt.plot(SoC[2][0:20],'k<-')
+    plt.ylabel('SoC')
+    plt.xlabel('Hour')
+    plt.show()
     
-#    return Ttank, Eused, Eloss, ElementOn, Eservice
+    plt.figure(3)
+    plt.clf()
+    plt.plot(ServiceCallsAccepted[0][0:20],'r*')
+    plt.plot(ServiceCallsAccepted[1][0:20],'b')
+    plt.plot(ServiceCallsAccepted[2][0:20],'k')
+    plt.ylabel('Service Calls Accepted')
+    plt.xlabel('Hour')
+    plt.show()
 
-
+    plt.figure(4)
+    plt.clf()
+    plt.plot(ServiceProvided[0][0:20],'r*')
+    plt.plot(ServiceProvided[1][0:20],'b')
+    plt.plot(ServiceProvided[2][0:20],'k')
+    plt.ylabel('Service Provided Per Timestep')
+    plt.xlabel('Hour')
+    plt.show()
+    
+    plt.figure(5)
+    plt.clf()
+    plt.plot(TotalServiceProvidedPerTimeStep[0:20],'r*-')
+    plt.ylabel('Total Service Provided by Fleet During Timestep')
+    plt.xlabel('Hour')
+    plt.show()
+    
+    plt.figure(6)
+    plt.clf()
+    plt.hist(TotalServiceProvidedPerWH)
+    plt.xlabel('Service Level, Watts')
+    plt.show()
+    
+    plt.figure(7)
+    plt.clf()
+    plt.hist(TotalServiceCallsAcceptedPerWH)
+    plt.xlabel('Total Service Calls Accepted per WH Annually')
+    plt.show()
 
 def get_annual_conditions():
         Tamb = []
--- simple_wh_cwb_2.py
+++ simple_wh_cwb_2.py
@@ -8,37 +8,39 @@ super simple water heater model
 
 
 class ChuckWaterHeater():
-    def __init__(self, Tset = 125, Tamb = 50, RHamb= 45, Tmains = 50, hot_draw = 0, control_signal = 'none', Capacity = 50, Type = 'ER', Location = 'Conditioned', service_calls_accepted = 0, max_service_calls = 100):
+    def __init__(self, Tamb = 50, RHamb= 45, Tmains = 50, hot_draw = 0, control_signal = 'none', Capacity = 50, Type = 'ER', Location = 'Conditioned', service_calls_accepted = 0, max_service_calls = 100):
         #Declare constants
-        self.Tset = Tset #F, specified so fleet can have a distribution of initial conditions
-        self.Tdeadband = 10 #delta F
+        self.Tdeadband = 1 #delta F
         self.E_heat = 4.5 #kW
         self.UA = 1 #W/K
-        self.control = control_signal
+#        self.control = control_signal
         self.Tmin = 105 # deg F
         self.Tmax = 160 # deg F
-#        self.Ttank = Ttank #deg F, specified so fleet can have a distribution of initial conditions
-#        print('ttank', Ttank)
-        self.Tamb = Tamb#deg F, specified so fleet can have a distribution of initial conditions
-        self.RHamb = RHamb# %, specified so fleet can have a distribution of initial conditions
-        self.Tmains = Tmains#deg F, specified so fleet can have a distribution of initial conditions
-        self.hot_draw = hot_draw#gpm, specified so fleet can have a distribution of initial conditions
+#        self.Tamb = Tamb#deg F, specified so fleet can have a distribution of initial conditions
+#        self.RHamb = RHamb# %, specified so fleet can have a distribution of initial conditions
+#        self.Tmains = Tmains#deg F, specified so fleet can have a distribution of initial conditions
+#        self.hot_draw = hot_draw#gpm, specified so fleet can have a distribution of initial conditions
         self.Capacity = Capacity # gallons
-        self.service_calls_accepted = int(service_calls_accepted)
+#        self.service_calls_accepted = int(service_calls_accepted)
         self.max_service_calls = int(max_service_calls)
+
         
-    def execute(self, Tset, Tamb, RHamb, Tmains, hot_draw, control_signal, Ttank):
+    def execute(self,Ttank, Tset, Tamb, RHamb, Tmains, hot_draw, control_signal, service_calls_accepted):
         Element_on = 0
-#        self.Ttank = Ttank #deg F, specified so fleet can have a distribution of initial conditions
-        (Tset, Ttank, Eused, Eloss, ElementOn, Eservice, SoC, AvailableCapacity, service_calls_accepted) = self.WH(self.Tset, Ttank,self.Tamb,self.Tmains,self.hot_draw, self.control, Element_on, self.service_calls_accepted, self.max_service_calls)
+
+        (Ttank, Tset, Eused, Eloss, ElementOn, Eservice, SoC, AvailableCapacity, service_calls_accepted) = self.WH(Tset, Ttank,Tamb,Tmains,hot_draw, control_signal, Element_on, service_calls_accepted, self.max_service_calls)
         
-        return Tset, Ttank, SoC, AvailableCapacity, service_calls_accepted
+        return Ttank, Tset, SoC, AvailableCapacity, service_calls_accepted, Eservice
 
     
-    def WH(self,Tset_ts, Tlast,Tamb_ts,Tmains_ts,hot_draw_ts, control_signal_ts, Element_on_ts, service_calls_accepted_ts, max_service_calls):
+    def WH(self,Tset, Tlast,Tamb_ts,Tmains_ts,hot_draw_ts, control_signal_ts, Element_on_ts, service_calls_accepted_ts, max_service_calls):
         """ Calculate next temperature and load"""
          
-        if Tlast < self.Tset - self.Tdeadband:
+        Eloss_ts = self.UA*(Tlast-Tamb_ts)    
+        dT_from_hot_draw = hot_draw_ts*60/50*(Tlast - Tmains_ts)
+        dT_loss = Eloss_ts*3600/(180*4810)
+        
+        if Tlast < Tset - self.Tdeadband:
             Eused_ts = self.E_heat*1000 #kW used
             Element_on_ts = 1
         elif Element_on_ts == 1 and Tlast< self.Tset:
@@ -50,33 +52,35 @@ class ChuckWaterHeater():
             
         #modify operation based on control signal    
         Eused_baseline_ts = Eused_ts # get baseline energy use w/o providing grid service
-        
-        if control_signal_ts  == 'load shed' and Tlast > self.Tmin and max_service_calls > service_calls_accepted_ts:
+#        print(Tlast,self.Tmin, max_service_calls, service_calls_accepted_ts )
+        if control_signal_ts[0]  == 'load shed' and Tlast > self.Tmin and max_service_calls > service_calls_accepted_ts and Element_on_ts == 1: #Element_on_ts = 1 requirement eliminates free rider situation
             Eused_ts = 0
             Element_on_ts = 0
             service_calls_accepted_ts += 1
-        elif control_signal_ts  == 'load shed' and Tlast < self.Tmin:
+#            print('shed','tlast=',Tlast,'tmin=', self.Tmin,'max calls=',max_service_calls,'calls accepted=',service_calls_accepted_ts)
+        elif control_signal_ts[0]  == 'load shed' and Tlast < self.Tmin:
             # don't change anything
             Eused_ts = Eused_ts
             Element_on_ts = Element_on_ts
-        elif control_signal_ts  == 'load add' and Tlast > self.Tmax:
+        elif control_signal_ts[0]  == 'load add' and Tlast > self.Tmax:
             Eused_ts = 0 #make sure it stays off
             Element_on_ts = 0
-        elif control_signal_ts  == 'load add' and Tlast < self.Tmax and max_service_calls > service_calls_accepted_ts:
+        elif control_signal_ts[0]  == 'load add' and Tlast < self.Tmax and max_service_calls > service_calls_accepted_ts and Element_on_ts == 0: #Element_on_ts = 0 requirement eliminates free rider situation
             #make sure it stays on
             Eused_ts = self.E_heat*1000 #kW used
             Element_on_ts = 1
             service_calls_accepted_ts += 1
+#            print('add','tlast',Tlast,'tmax=', self.Tmax,'max calls=',max_service_calls,'calls accepted=',service_calls_accepted_ts)
         
-        #calculate energy provided as a service
+        #calculate energy provided as a service, >0 is load add, <0 load shed
         Eservice_ts = Eused_ts-Eused_baseline_ts
         
         #could change this at some point based on signals
-        Tset_ts = self.Tset
-            
-        Eloss_ts = self.UA*(Tlast-Tamb_ts)    
-        Ttank_ts = Tlast + (Eused_ts - Eloss_ts)*3600/(180*4810) + (1 + hot_draw_ts*60/50*(Tmains_ts - Tlast)) 
-        print('tlast', Tlast)
+        Tset_ts = Tset
+#        print(service_calls_accepted_ts)    
+        
+        dT_power_input = Eused_ts*3600/(180*4810)
+        Ttank_ts = Tlast + dT_power_input - dT_loss - dT_from_hot_draw 
         #3600 converts W to Whrs, 180 is kg of water in 50ga tank, 4810 is heat cap of water
         # second expression in line above is new mixed temperature due to water draw only
         
@@ -84,7 +88,7 @@ class ChuckWaterHeater():
         isAvailable = 1 if max_service_calls-service_calls_accepted_ts > 0 else 0
         Available_Capacity = SOC*self.Capacity/3.79*4180*(self.Tmax - self.Tmin)*isAvailable #Joules, 3.79 = kg/gal, 4180 heat cap of water J/kgK
         
-        return Tset_ts, Ttank_ts, Eused_ts, Eloss_ts, Element_on_ts, Eservice_ts, SOC, Available_Capacity, service_calls_accepted_ts
+        return Ttank_ts, Tset_ts, Eused_ts, Eloss_ts, Element_on_ts, Eservice_ts, SOC, Available_Capacity, service_calls_accepted_ts
     
-#if __name__ == '__main__':
-#    main()
+if __name__ == '__main__':
+    main()
